0e9848c26930b95b623c65232faf201c88519b76
M4sum <masumpatel39@gmail.com>
2019-12-06 16:26:45 +0530

    buildpacks added

--- Procfile
+++ Procfile
@@ -1,2 +1,4 @@
 web: gunicorn app:app
+web: gunicorn --bind 0.0.0.0:${PORT} wsgi
+web: export FLASK_ENV=development
 web: flask run
--- app.py
+++ app.py
@@ -35,3 +35,8 @@ def function():
   return render_template(
         'date.html', date = date
     )
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
